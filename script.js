de6c76ccc7ab4745e85693936e573ed5adffea3c
henrygagnier <101131194+henrygagnier@users.noreply.github.com>
2023-05-13 12:16:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const axios = require("axios");
+import axios from "axios";
 
 axios.get("https://api.geckoterminal.com/api/v2/networks/bsc/pools/0x8A7292E9ba068ec5221e4b5d491251a5b06762CC")
   .then(response => {
     const price = response.data.data.attributes.base_token_price_usd * 1000000;
-    console.log(price);
     document.getElementById("price").textContent = String(price);
   })
   .catch(error => {
